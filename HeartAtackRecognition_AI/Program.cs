58941e66eb00e15f24d01167647e97b4152f532f
Wojciech Gąsiewicz <235086@student.pwr.edu.pl>
2019-07-12 22:40:38 +0200

    start implementing ml algorithm

--- HeartAtackRecognition_AI/Program.cs
+++ HeartAtackRecognition_AI/Program.cs
@@ -3,11 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.ML;
 
 namespace HeartAtackRecognition_AI
 {
+    class FeedBackTrainingData
+    {
+        public string FeedBackText { get; set; }
+        public bool IsGood { get; set; }
+
+    }
     class Program
     {
+        static List<FeedBackTrainingData> trainingData = new List<FeedBackTrainingData>();
+        static void LoadTraingingData()
+        {
+            trainingData.Add(new FeedBackTrainingData()
+            {
+                FeedBackText = "this is good",
+                IsGood=true
+            });
+            trainingData.Add(new FeedBackTrainingData()
+            {
+                FeedBackText = "this is horrible",
+                IsGood = true
+            });
+        }
         static void Main(string[] args)
         {
         }
