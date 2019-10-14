a8f20a6eaf6bb267141a5de2c4266e3a73e50713
Wojciech Gąsiewicz <235086@student.pwr.edu.pl>
2019-10-14 23:03:32 +0200

    heartattack model pass wrong values

---
+++ CardiologicClinic_WebApp/AI/main.py
@@ -0,0 +1,119 @@
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.callbacks import History
+import numpy as np
+import matplotlib.pyplot as plt
+# fix random seed for reproducibility
+np.random.seed(7)
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from keras import optimizers
+
+def Save():
+    model.save('medical_module.h5')
+
+def PredictPatientData():    
+    # load input
+    data = np.loadtxt('./predict.txt', delimiter=";", dtype=np.int)
+    predict = model.predict_classes(np.array([data,]))
+    print(predict)
+
+def DrawPlots():    
+    # summarize history for accuracy
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+    # summarize history for loss
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+#read data
+data = pd.read_csv("./danecsvintH.csv", sep=";")
+
+#delete rows with missing data
+#data = data[data.ca != '?']
+#data = data[data.thal != '?']
+
+#split data to training and test set (80-20)
+X_train_df, X_test_df = train_test_split(data, test_size=0.2, shuffle=True)
+
+#creating output classes
+Y_train_df = X_train_df['num']
+Y_test_df = X_test_df['num']
+
+#deleting output classes from training data
+X_train_df = X_train_df.drop(['num'], axis = 1)
+X_test_df = X_test_df.drop(['num'], axis = 1)
+
+#converting from datafram to numpy array
+X_train = X_train_df.values
+X_test = X_test_df.values
+Y_train = Y_train_df.values
+Y_test = Y_test_df.values
+
+#changing values from 1,2,3,4 of illness to 1
+#Y_train[Y_train > 0] = 1
+#Y_test[Y_test > 0] = 1
+
+#casting values to int
+X_train = X_train.astype('int')
+X_test = X_test.astype('int')
+Y_train = Y_train.astype('int')
+Y_test = Y_test.astype('int')
+
+# create model
+model = Sequential()
+
+# records events into a History object
+history = History()
+
+#layers of the model
+# relu -> activation(x) = max(0,x)
+model.add(Dense(13, input_dim=59, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(8, activation='relu'))
+#model.add(Dense(5, activation='relu'))
+#model.add(Dense(5, activation='relu'))
+#model.add(Dense(1, activation='sigmoid'))
+model.add(Dense(5, activation='sigmoid'))
+
+#optymalizer settings, in this examlpe we changed default learning rate
+adam = optimizers.Adam(lr=0.002)
+
+#configurate model and picking optymalizer, loss function and metrics.
+model.compile(loss='sparse_categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
+
+# Start learning
+# batch_size is number of samples per gradient update
+# epoch is an iteration over the entire data provided
+#fit - train data
+
+model.fit(X_train, Y_train, epochs=150, batch_size=10, callbacks=[history])
+
+# evaluate the model
+# acc = number of correct pred / total num of pred
+scores = model.evaluate(X_test, Y_test)
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
+print(model.summary())
+Save()
+
+tocheck = X_test[0]
+predict = model.predict_classes(np.array([tocheck,]))
+print(predict)
+tocheck = X_test[2]
+predict = model.predict_classes(np.array([tocheck,]))
+print(predict)
+
+PredictPatientData()
+
+DrawPlots()
--- CardiologicClinic_WebApp/Controllers/HomeController.cs
+++ CardiologicClinic_WebApp/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using CardiologicClinic_WebApp.Models;
 using HearAttackRecognition_ML.Models;
 using CardiologicClinic_WebApp.Views.Home;
+using System.IO;
 
 namespace CardiologicClinic_WebApp.Controllers
 {
@@ -60,6 +61,27 @@ namespace CardiologicClinic_WebApp.Controllers
 
         public IActionResult Diagnosis(HeartAttackModel.InputModel Input)
         {
+            string predict = Input.Age.ToString() + ";" + (Input.Sex ? 1 : 0).ToString() + ";" + Input.PainLocation.ToString() + ";" + Input.ChestPainRadiation.ToString()
+                + ";" + Input.PainCharacter.ToString() + ";" + Input.OnsetOfPain.ToString() + ";" + Input.NumOfHoursSinceOnset.ToString()
+                + ";" + Input.DurationOfTheLastEpisode.ToString() + ";" + (Input.Nausea ? 1 : 0).ToString() + ";" + (Input.Diaphoresis ? 1 : 0).ToString()
+                + ";" + (Input.Palpitations ? 1 : 0).ToString() + ";" + (Input.Dyspnea ? 1 : 0).ToString() + ";" + (Input.Dizziness ? 1 : 0).ToString()
+                + ";" + (Input.Burping ? 1 : 0).ToString() + ";" + Input.PalliativeFactors.ToString() + ";" + Input.PriorChestPainOfThisType.ToString()
+                + ";" + Input.PhysicianConsultedForPriorPain.ToString() + ";" + Input.PriorPainRelatedToHeart.ToString() + ";" + Input.PriorPainDueToMI.ToString()
+                + ";" + Input.PriorPainDueToAnginaPectoris.ToString() + ";" + Input.PriorMI.ToString() + ";" + Input.PriorAnginaPectoris.ToString()
+                + ";" + Input.PriorAntypicalChestPain.ToString() + ";" + Input.CongestiveHeartFailure.ToString() + ";" + Input.PeripheralVascularFailure.ToString()
+                + ";" + Input.HiatalHernia.ToString() + ";" + Input.Hypertension.ToString() + ";" + Input.Diabetes.ToString() + ";" + Input.Smoker.ToString()
+                + ";" + Input.Diuretics.ToString() + ";" + Input.Nitrates.ToString() + ";" + Input.BetaBlockers.ToString() + ";" + Input.Digitalis.ToString()
+                + ";" + Input.Nonsteroidal.ToString() + ";" + Input.Antacids.ToString() + ";" + Input.SystolicBloodPressure.ToString() + ";" + Input.DiastolicBloodPressure.ToString()
+                + ";" + Input.HeartRate.ToString() + ";" + Input.RespirationRate.ToString() + ";" + Input.Rales.ToString() + ";" + Input.Cyanosis.ToString()
+                + ";" + Input.Pallor.ToString() + ";" + Input.SystolicMurmur.ToString() + ";" + Input.DiastolicMurmur.ToString() + ";" + Input.Oedema.ToString()
+                + ";" + Input.S3Gallop.ToString() + ";" + Input.S4Gallop.ToString() + ";" + Input.ChestWallTenderness.ToString() + ";" + Input.DiaphoresisPE.ToString()
+                + ";" + Input.NewQWave.ToString() + ";" + Input.AnyQWave.ToString() + ";" + Input.NewSTSegmentElevation.ToString() + ";" + Input.AnySTSegmentElevation.ToString()
+                + ";" + Input.NewSTSegmentDepression.ToString() + ";" + Input.AnySTSegmentDepression.ToString() + ";" + Input.NewTWaveInversion.ToString()
+                + ";" + Input.AnyTWaveInversion.ToString() + ";" + Input.NewIntraventricularConductionDefect.ToString() + ";" + Input.AnyIntraventricularConductionDefect.ToString();
+            using (StreamWriter wr = new StreamWriter("predict.txt"))
+            {
+                wr.WriteLine(predict);
+            }
             HeartData userData = new HeartData()
             {
                 Age = Input.Age,
